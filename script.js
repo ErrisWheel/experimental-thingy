98159943e517407b2dd8fa230a155a6b9f0419fb
ErrisWheel <errisdelrosario@gmail.com>
2025-08-01 05:11:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 console.log("You are now hacked. JK -E")
 
 const layer = document.querySelector('.parallax-layer');
+const objects = document.querySelectorAll('.parallax-object');
 
 window.addEventListener('mousenive', (e) => {
     const xRatio = (e.clientX / window.innerWidth) * 2 - 1;
     const yRatio = (e.clientY / window.innerHeight) * 2 - 1;
 
-    const movementStrength = 30;
+    const bgStrength = 10;
+    layer.style.transform = `translate3d(${-xRatio * bgStrength}px, ${-yRatio * bgStrength}px, 0)`;
 
-    const xMove = -xRatio * movementStrength;
-    const yMove = -yRatio * movementStrength;
+    objects.forEach(obj => {
+        let strength;
+        if (obj.classList.contains('bat')) strength = 40;
+        else if (obj.classList.contains('wolf')) strength = 20;
 
-    layer.style.transform = `translate3d(${xMove}px, ${yMove}px, 0)`;
+        const xMove = -xRatio * strength;
+        const yMove = -yRatio * strength;
+        obj.style.transform = `translate3d(${xMove}px, ${yMove}px, 0)`;
+    });
 });
