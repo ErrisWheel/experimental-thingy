b5522f51465a152c2c74576783676821fce12d19
ErrisWheel <errisdelrosario@gmail.com>
2025-08-01 04:54:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("You are now hacked. JK -E")
+
+const layer = document.querySelector('.parallax-layer');
+
+window.addEventListener('mousenive', (e) => {
+    const xRatio = (e.clientX / window.innerWidth) * 2 - 1;
+    const yRatio = (e.clientY / window.innerHeight) * 2 - 1;
+
+    const movementStrength = 30;
+
+    const xMove = -xRatio * movementStrength;
+    const yMove = -yRatio * movementStrength;
+
+    layer.style.transform = `translate3d(${xMove}px, ${yMove}px, 0)`;
+});
